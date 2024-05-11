a7700015171b3c613b4bde40b703d4c52bf86f6b
J.G <54757200+jingul414@users.noreply.github.com>
2024-05-11 21:00:12 +0900

    Add files via upload

--- 10주차/도전과제#9/Driver.java
+++ 10주차/도전과제#9/Driver.java
@@ -1,14 +1,15 @@
-package subject9_3;
+package subject_c;
 
 public class Driver {
     public static void main(String[] args) {
-        Course[] courses = new Course[5];
+        final int MAX_SIZE = 5;
+        Course[] courses = new Course[MAX_SIZE];
         courses[0] = new Major("자바", "CS101", "컴퓨터공학", "C");
         courses[1] = new Major("이산 수학", "CS215", "컴퓨터공학", "없음");
         courses[2] = new GenEdu("물리학", "PHS210", "학문기초교양");
         courses[3] = new GenEdu("영어 1", "ENG101", "공통교양");
         courses[4] = new Elective("테니스", "REC310");
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < MAX_SIZE; i++){
             courses[i].printInfo();
             System.out.println("");
         }
